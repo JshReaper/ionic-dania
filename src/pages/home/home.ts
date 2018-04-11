0d55384329c54fe48f60e0e6f589b40fb741a21b
Topper <tobi1995@live.dk>
2018-04-11 11:25:57 +0200

    commit picture

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, Content } from 'ionic-angular';
+import { NavController, NavParams, Content, AlertController } from 'ionic-angular';
 import { RoomPage } from '../room/room';
 import * as firebase from 'Firebase';
 import {Camera , CameraOptions, DestinationType, EncodingType, MediaType} from '@ionic-native/camera'
@@ -20,11 +20,12 @@ export class HomePage {
   offStatus:boolean = false;
   public imageToShow:any;
   isImageLoading:boolean = true;
+  alertCtrl : AlertController;
 
   public photos : any;
   public base64Image : string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private camera : Camera) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera : Camera, alertCtrl:AlertController) {
+    this.alertCtrl = alertCtrl;
     this.roomkey = this.navParams.get("key") as string;
     this.nickname = this.navParams.get("nickname") as string;
     this.data.type = 'message';
@@ -63,6 +64,17 @@ export class HomePage {
     this.data.message = '';
     this.imageToShow = '';
   }
+  sendPicture() {
+    let newData = firebase.database().ref('chatrooms/'+this.roomkey+'/chats').push();
+    newData.set({
+      type:this.data.type,
+      user:this.data.nickname,
+      picture:this.base64Image,
+      sendDate:Date()
+    });
+    this.data.message = '';
+    this.imageToShow = '';
+  }
   exitChat() {
     let exitData = firebase.database().ref('chatrooms/'+this.roomkey+'/chats').push();
     exitData.set({
@@ -87,18 +99,48 @@ export class HomePage {
       mediaType: this.camera.MediaType.PICTURE
     }
     this.camera.getPicture(this.cameraOptions).then((imageData) =>{
-this.isImageLoading = true;
-this.base64Image = "data:image/jpeg;base64," + imageData;
-      this.photos.push(this.base64Image);
-      this.photos.reverse();
-this.isImageLoading = false;
+    this.isImageLoading = true;
+    this.base64Image = "data:image/jpeg;base64," + imageData;
+    this.upload();
+    this.isImageLoading = false;
     },function(err){
       console.log(err);
     });
     
     console.log("Camera button event detected");
   }
+
+  upload(){
+    let storageRef = firebase.storage().ref();
+
+    //timestamp as filename
+    const fileName = Math.floor(Date.now() / 1000);
+
+    //creating a reffrence to images/todays-date.jpg
+    const imageRef = storageRef.child('images/${filename}.jpg');
+
+    imageRef.putString(this.base64Image, firebase.storage.StringFormat.DATA_URL).then((snapshot)=>{
+
+      //succesful upload
+      this.showSuccesfulUploadAlert();
+
+    });
+  };
+  showSuccesfulUploadAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Picture posted',
+      subTitle: 'Picture was successfully posted!',
+      buttons: ['OK']
+    });
+    alert.present();
+    this.sendPicture();
+    // clear the previous photo data in the variable
+    this.base64Image = "";
+  };
+
 };
+
+
 
  
 
